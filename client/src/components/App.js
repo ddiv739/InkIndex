8734b03f862760f53fff98478b117fb18e5bb5d7
Antonio <toncas@windowslive.com>
2018-05-13 15:28:12 -0700

    [ops] server and client communicate with each other

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Hello from React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
